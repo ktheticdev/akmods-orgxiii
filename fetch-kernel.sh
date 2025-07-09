799c351a8112536a4ecce02427470d6b128dca05
vindiesel <anton.perstenyov@gmail.com>
2025-07-09 22:06:35 +0300

    try switching to CachyOS

--- fetch-kernel.sh
+++ fetch-kernel.sh
@@ -41,6 +41,9 @@ case "$kernel_flavor" in
         ;;
     "centos-hsk")
         ;;
+    "cachy")
+	dnf copr enable -y bieszczaders/kernel-cachyos
+	;;
     "main")
         ;;
     *)
@@ -96,6 +99,15 @@ elif [[ "${kernel_flavor}" == "centos-hsk" ]]; then
     dnf download -y --enablerepo="centos-hyperscale" \
         kernel-"${kernel_version}" \
         kernel-core-"${kernel_version}" \
+        kernel-modules-"${kernel_version}" \
+        kernel-modules-core-"${kernel_version}" \
+        kernel-modules-extra-"${kernel_version}" \
+        kernel-devel-"${kernel_version}" \
+        kernel-devel-matched-"${kernel_version}" \
+        kernel-uki-virt-"${kernel_version}"
+elif [[ "${kernel_flavor}" == "cachy" ]]; then
+    dnf download -y \
+	kernel-"${kernel_version}" \
         kernel-modules-"${kernel_version}" \
         kernel-modules-core-"${kernel_version}" \
         kernel-modules-extra-"${kernel_version}" \
