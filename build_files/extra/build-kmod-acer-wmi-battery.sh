bab07caf3360f3632a34dafbc7fc694403de44df
JoViatrix <62498297+JoViatrix@users.noreply.github.com>
2025-10-28 17:22:51 +0000

    Update build-kmod-acer-wmi-battery.sh

--- build_files/extra/build-kmod-acer-wmi-battery.sh
+++ build_files/extra/build-kmod-acer-wmi-battery.sh
@@ -7,13 +7,11 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-COPR_RELEASE="${RELEASE}"
+COPR_RELEASE="rawhide"
 
 curl -LsSf -o /etc/yum.repos.d/_copr_asan-acer-modules.repo "https://copr.fedorainfracloud.org/coprs/asan/acer-modules/repo/fedora-${COPR_RELEASE}/asan-acer-modules-fedora-${COPR_RELEASE}.repo"
 
 ### BUILD acer-wmi-battery (succeed or fail-fast with debug output)
-
-ls ./
 dnf install -y \
     akmod-acer-wmi-battery-*.fc${RELEASE}.${ARCH}
 akmods --force --kernels "${KERNEL}" --kmod acer-wmi-battery
