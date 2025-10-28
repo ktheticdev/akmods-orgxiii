335e19568e026f9a3d986ac5985784c33cf9636e
JoViatrix <62498297+JoViatrix@users.noreply.github.com>
2025-10-28 17:34:18 +0000

    Update build-kmod-acer-wmi-battery.sh

--- build_files/extra/build-kmod-acer-wmi-battery.sh
+++ build_files/extra/build-kmod-acer-wmi-battery.sh
@@ -13,7 +13,7 @@ curl -LsSf -o /etc/yum.repos.d/_copr_asan-acer-modules.repo "https://copr.fedora
 
 ### BUILD acer-wmi-battery (succeed or fail-fast with debug output)
 dnf install -y \
-    akmod-acer-wmi-battery-*.fc${RELEASE}.${ARCH}
+    akmod-acer-wmi-battery-*.fc${COPR_RELEASE}.${ARCH}
 akmods --force --kernels "${KERNEL}" --kmod acer-wmi-battery
 modinfo /usr/lib/modules/${KERNEL}/extra/acer-wmi-battery/acer-wmi-battery.ko.xz > /dev/null \
 || (find /var/cache/akmods/acer-wmi-battery/ -name \*.log -print -exec cat {} \; && exit 1)
