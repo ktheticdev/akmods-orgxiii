4fc337157f4de6710bdc6fcb662b9de9719fa15b
JoViatrix <62498297+JoViatrix@users.noreply.github.com>
2025-10-28 16:29:01 +0000

    Update build-kmod-vhba.sh

--- build_files/extra/build-kmod-vhba.sh
+++ build_files/extra/build-kmod-vhba.sh
@@ -7,12 +7,6 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-if [[ "${RELEASE}" -ge 43 ]]; then
-    COPR_RELEASE="rawhide"
-else
-    COPR_RELEASE="${RELEASE}"
-fi
-
 curl -LsSf -o /etc/yum.repos.d/_copr_rok-cdemu.repo "https://copr.fedorainfracloud.org/coprs/rok/cdemu/repo/fedora-${COPR_RELEASE}/rok-cdemu-fedora-${COPR_RELEASE}.repo"
 
 ### BUILD vhba (succeed or fail-fast with debug output)
