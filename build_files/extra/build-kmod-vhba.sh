0c3a4405fdd5459edc1b022353bdcb6e7df843f2
JoViatrix <62498297+JoViatrix@users.noreply.github.com>
2025-10-28 16:42:29 +0000

    Update build-kmod-vhba.sh

--- build_files/extra/build-kmod-vhba.sh
+++ build_files/extra/build-kmod-vhba.sh
@@ -7,6 +7,8 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
+COPR_RELEASE="${RELEASE}"
+
 curl -LsSf -o /etc/yum.repos.d/_copr_rok-cdemu.repo "https://copr.fedorainfracloud.org/coprs/rok/cdemu/repo/fedora-${COPR_RELEASE}/rok-cdemu-fedora-${COPR_RELEASE}.repo"
 
 ### BUILD vhba (succeed or fail-fast with debug output)
